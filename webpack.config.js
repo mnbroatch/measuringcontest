0de9b46854bd3272d4a4ec11d20d8fbd05057c27
Matthew Broatch <mnbroatch@gmail.com>
2025-07-02 17:31:45 -0700

    cache buster, lambda deploy environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
+class CacheBustPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('CacheBustPlugin', (compilation) => {
+      HtmlWebPackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync(
+        'CacheBustPlugin',
+        (data, cb) => {
+          const appendQuery = (tag) => {
+            if (tag.tagName === 'script' && tag.attributes && tag.attributes.src) {
+              tag.attributes.src += `?v=${Date.now()}`;
+            }
+          };
+
+          data.headTags.forEach(appendQuery);
+          data.bodyTags.forEach(appendQuery);
+
+          cb(null, data);
+        }
+      );
+    });
+  }
+}
+
 module.exports = {
   entry: './index.js',
   output: {
-    filename: 'build.js'
+    filename: 'build.js',
+    chunkFilename: '[name].js?cacheBust=[chunkhash]'
   },
   optimization: {
     minimize: false
@@ -11,8 +34,12 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       title: 'Measuring Contest',
-      template: './index.html'
-    })
+      template: './index.html',
+      templateParameters: {
+        version: Date.now(),
+      },
+    }),
+    new CacheBustPlugin()
   ],
   module: {
     rules: [
