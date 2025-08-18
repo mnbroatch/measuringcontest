164c9411b2122fc6bea3d9b75ea0dfd4fe1b2f6f
Matthew Broatch <mnbroatch@gmail.com>
2025-08-18 15:13:28 -0700

    delete function, remove unused cfn

--- lambda/delete-session/index.js
+++ lambda/delete-session/index.js
@@ -1,93 +1,51 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb")
-const { DynamoDBDocumentClient, UpdateCommand, TransactWriteCommand } = require("@aws-sdk/lib-dynamodb")
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, GetCommand, TransactWriteCommand } = require("@aws-sdk/lib-dynamodb");
 
-const client = new DynamoDBClient()
-const dynamoDb = DynamoDBDocumentClient.from(client)
-const MAX_SESSIONS_PER_USER = 1
-const MAX_SESSIONS_ERROR_MESSAGE = 'You have reached the maximum number of sessions allowed.'
-const MAX_SESSIONS_ERROR_NAME = 'MAX_SESSIONS_ERROR'
+const client = new DynamoDBClient();
+const dynamoDb = DynamoDBDocumentClient.from(client);
 
-exports.handler = async (event) => {
-  try {
-    const session = await createSession(await getSessionCode(), event.requestContext.authorizer.claims.sub)
-    return { val: session }
-  } catch (error) {
-    let errorMessage = error.message
-    let errorName = error.name
-    if (error.name === "TransactionCanceledException" && error.message.includes("ConditionalCheckFailed")) {
-      errorMessage = MAX_SESSIONS_ERROR_MESSAGE
-      errorName = MAX_SESSIONS_ERROR_NAME
-    }
-    return {
-      errorMessage,
-      errorName,
-    }
+async function deleteSession(sessionCode, creatorUserId) {
+  const { Item } = await dynamoDb.send(
+    new GetCommand({
+      TableName: "measuringcontest-users",
+      Key: { userId: creatorUserId },
+      ProjectionExpression: "sessions"
+    })
+  );
+
+  if (!Item?.sessions || !Array.isArray(Item.sessions)) {
+    throw new Error("No sessions found for this user");
   }
-}
 
-async function createSession (sessionCode, userId) {
+  const index = Item.sessions.indexOf(sessionCode);
+  if (index === -1) throw new Error("Session not found in creator's list");
+
   const params = {
     TransactItems: [
       {
         Update: {
           TableName: "measuringcontest-users",
-          Key: { userId },
-          UpdateExpression: 'SET sessions = list_append(if_not_exists(sessions, :emptyList), :newSession), createdAt = if_not_exists(createdAt, :now)',
-          ConditionExpression: "attribute_not_exists(sessions) OR size(sessions) < :maxSessions",
+          Key: { userId: creatorUserId },
+          UpdateExpression: `REMOVE sessions[${index}]`,
+          ConditionExpression: "contains(sessions, :code)",
           ExpressionAttributeValues: {
-            ":newSession": [sessionCode],
-            ":emptyList": [],
-            ":now": Date.now(),
-            ":maxSessions": MAX_SESSIONS_PER_USER,
+            ":code": sessionCode
           }
         }
       },
       {
-        Put: {
+        Delete: {
           TableName: "measuringcontest-sessions",
-          Item: {
-            sessionCode,
-            createdBy: userId,
-            members: [userId],
-            sessionStatus: "waiting",
-            createdAt: Date.now(),
-            expiresAtSeconds: Math.floor(Date.now() / 1000) + 24 * 3600
-          },
-          ConditionExpression: "attribute_not_exists(sessionCode)"
+          Key: { sessionCode },
+          ConditionExpression: "attribute_exists(sessionCode)"
         }
       }
     ]
   };
 
   await dynamoDb.send(new TransactWriteCommand(params));
+
   return { success: true, sessionCode };
 }
 
-async function getSessionCode () {
-  const  sessionCounter = (await dynamoDb.send(
-    new UpdateCommand({
-      TableName: "measuringcontest-global",
-      Key: {
-        property: "sessioncounter",
-      },
-      UpdateExpression: "ADD val :inc",
-      ExpressionAttributeValues: {
-        ":inc": 1,
-      },
-      ReturnValues: "UPDATED_NEW",
-    })
-  )).Attributes.val
-
-
-  return encodeAlphaCode(sessionCounter)
-}
-
-function encodeAlphaCode(num) {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  let code = "";
-  for (let i = 0; i < 4; i++) {
-    code = alphabet[num % 26] + code;
-    num = Math.floor(num / 26);
-  }
-  return code;
-}
+module.exports = { deleteSession };
