7187a153187d112d980aa3763475e011b72bed59
Matthew Broatch <mnbroatch@gmail.com>
2025-08-20 12:47:17 -0700

    delete session lambda handler

--- lambda/delete-session/index.js
+++ lambda/delete-session/index.js
@@ -4,6 +4,28 @@ const { DynamoDBDocumentClient, GetCommand, TransactWriteCommand } = require("@a
 const client = new DynamoDBClient();
 const dynamoDb = DynamoDBDocumentClient.from(client);
 
+exports.handler = async (event) => {
+  try {
+    const session = await deleteSession(
+      event.pathParameters.sessionCode,
+      event.requestContext.authorizer.claims.sub
+    );
+    return { val: session };
+  } catch (error) {
+    let errorMessage = error.message;
+    let errorName = error.name;
+
+    if (errorName === "TransactionCanceledException" && errorMessage.includes("ConditionalCheckFailed")) {
+      errorMessage = "Session already deleted or does not exist";
+    }
+
+    return {
+      errorMessage,
+      errorName,
+    };
+  }
+};
+
 async function deleteSession(sessionCode, creatorUserId) {
   const { Item } = await dynamoDb.send(
     new GetCommand({
