898f40ec003c9d6ca814525cdf008e655a236475
Matthew Broatch <mnbroatch@gmail.com>
2025-09-16 14:04:42 -0700

    lambda debugging

--- lambda/create-game/index.js
+++ lambda/create-game/index.js
@@ -33,9 +33,11 @@ exports.handler = async (event) => {
   const gameId = `game-${Math.random().toString(36).slice(2, 10)}`;
 
   let createResp
+  let x = 1
   try {
     const testResp = await fetch('https://httpbin.org/get');
     console.log('Test fetch successful:', testResp.ok);
+    x = 2
     // Create boardgame.io game
     createResp = await fetch(`${BOARDGAME_SERVER_URL}/games/${body.gameName}/create`, {
       method: "POST",
@@ -43,7 +45,7 @@ exports.handler = async (event) => {
       body: JSON.stringify({ numPlayers: body.numPlayers || 2 }),
     });
   } catch (e) {
-    return e
+    return { ...e, x }
   }
   const createData = await createResp.json();
   if (!createResp.ok) {
