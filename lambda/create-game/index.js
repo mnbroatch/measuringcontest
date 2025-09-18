5e484b5446b48a61d3eb384f8a3652a131f74f9a
Matthew Broatch <mnbroatch@gmail.com>
2025-09-18 12:22:54 -0700

    lambda debug

--- lambda/create-game/index.js
+++ lambda/create-game/index.js
@@ -69,6 +69,7 @@ exports.handler = async (event) => {
 
   let createData 
 try {
+  console.log(`${BOARDGAME_SERVER_URL}/games/${body.gameName}/create`)
   const createResp = await fetch(`${BOARDGAME_SERVER_URL}/games/${body.gameName}/create`, {
     method: "POST",
     headers: { 
