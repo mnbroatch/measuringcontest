ef1088637eac8d170f9b075073010343f38355eb
Matthew Broatch <mnbroatch@gmail.com>
2025-09-26 23:00:35 -0700

    wip

--- server/game-factory/registry.js
+++ server/game-factory/registry.js
@@ -1,7 +1,7 @@
 import Spaces from "./spaces/spaces.js";
 import Space from "./space/space.js";
 import Grid from "./spaces/grid.js";
-import Piece from "./piece/piece.ts";
+import Piece from "./piece/piece.js";
 import Bank from "./piece/bank.js";
 import BankSlot from "./piece/bank-slot.js";
 
