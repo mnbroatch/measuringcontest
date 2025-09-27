a3162e50a8d4a1e4d30421ab093dcd7b3dfcfaff
Matthew Broatch <mnbroatch@gmail.com>
2025-09-27 13:54:03 -0700

    wip

--- src/game-factory/entity.js
+++ src/game-factory/entity.js
@@ -1,7 +1,7 @@
 export default class Entity {
-  constructor (rule, id, { fromFactory }) {
-    if (!fromFactory) {
-      throw new Error(`Do not create entities directly. Go through the Bank.`)
+  constructor (rule, id, options) {
+    if (!options?.fromFactory) {
+      throw new Error(`Do not create entities directly. Go through the Bank. rule: ${rule}`)
     }
     this.rule = rule
     this.entityId = id
