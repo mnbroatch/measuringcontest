d5d13e4492bc8565ea9845a2146777a8d2dbd311
Matthew Broatch <mnbroatch@gmail.com>
2025-09-27 02:05:26 -0700

    tags

--- server/game-factory/condition/does-not-contain-condition.js
+++ server/game-factory/condition/does-not-contain-condition.js
@@ -2,7 +2,7 @@ import matches from "lodash/matches.js";
 import Condition from "../condition/condition.js";
 
 export default class DoesNotContainCondition extends Condition {
-  isMet(_, __, payload) {
+  isMet(_, payload) {
     const { space } = this.resolveMappings(payload)
     return !space.pieces.filter(matches(this.rule.piece)).length;
   }
