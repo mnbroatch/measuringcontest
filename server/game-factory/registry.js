1a767d902e7aac66a83134c5444440edf7679d8e
Matthew Broatch <mnbroatch@gmail.com>
2025-09-26 22:57:07 -0700

    wip

--- server/game-factory/registry.js
+++ server/game-factory/registry.js
@@ -1,11 +1,13 @@
 import Spaces from "./spaces/spaces.js";
+import Space from "./space/space.js";
 import Grid from "./spaces/grid.js";
-import Piece from "./piece/piece.js";
+import Piece from "./piece/piece.ts";
 import Bank from "./piece/bank.js";
 import BankSlot from "./piece/bank-slot.js";
 
 export const registry = {
   Spaces,
+  Space,
   Grid,
   Piece,
   Bank,
--- src/game-factory/registry.js
+++ src/game-factory/registry.js
@@ -1,4 +1,5 @@
 import Spaces from "./spaces/spaces.js";
+import Space from "./space/space.js";
 import Grid from "./spaces/grid.js";
 import Piece from "./piece/piece.ts";
 import Bank from "./piece/bank.js";
@@ -6,6 +7,7 @@ import BankSlot from "./piece/bank-slot.js";
 
 export const registry = {
   Spaces,
+  Space,
   Grid,
   Piece,
   Bank,
