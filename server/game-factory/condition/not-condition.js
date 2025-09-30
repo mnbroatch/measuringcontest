9a472c8c9e3fb9393741fc70506f0cc30f04590f
Matthew Broatch <mnbroatch@gmail.com>
2025-09-30 00:24:49 -0700

    aliver?

--- server/game-factory/condition/not-condition.js
+++ server/game-factory/condition/not-condition.js
@@ -3,9 +3,10 @@ import conditionFactory from "./condition-factory.js";
 
 export default class NotCondition extends Condition {
   checkCondition(...args) {
-    const conditionIsMet = !this.rule.conditions.every(conditionRule =>
-      conditionFactory(conditionRule).isMet(...args)
-    )
+    const conditionIsMet = !this.rule.conditions.every(conditionRule => {
+      const x = conditionFactory(conditionRule).isMet(...args)
+      console.log('x', x)
+    })
     return { conditionIsMet }
   }
 }
