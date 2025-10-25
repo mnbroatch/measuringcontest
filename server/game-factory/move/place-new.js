811f4236aa7b93a6b72d89ac2ed3a599839652f5
Matthew Broatch <mnbroatch@gmail.com>
2025-10-24 23:35:18 -0700

    wip

--- server/game-factory/move/place-new.js
+++ server/game-factory/move/place-new.js
@@ -2,9 +2,11 @@ import Move from "./move.js";
 
 export default class PlaceNew extends Move {
   do(bgioArguments, { arguments: { destination } }, context) {
-    const entity = this.rule.matchMultiple
-      ? bgioArguments.G.bank.getMultiple(bgioArguments, this.rule.entity, context)
-      : bgioArguments.G.bank.getOne(bgioArguments, this.rule.entity, context)
-    destination.placeEntity(entity, this.rule.position)
+    const entities = this.rule.matchMultiple
+      ? bgioArguments.G.bank.getMultiple(bgioArguments, this.rule.entity, this.rule.count, context)
+      : [bgioArguments.G.bank.getOne(bgioArguments, this.rule.entity, context)]
+    entities.forEach((entity) => {
+      destination.placeEntity(entity, this.rule.position)
+    })
   }
 }
