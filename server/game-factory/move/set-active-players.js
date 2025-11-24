eee090891bc6b002ed3e4a6bf87fb7c96b225ccb
Matthew Broatch <mnbroatch@gmail.com>
2025-11-24 14:51:35 -0800

    wip

--- server/game-factory/move/set-active-players.js
+++ server/game-factory/move/set-active-players.js
@@ -5,16 +5,12 @@ export default class SetActivePlayers extends Move {
   do(bgioArguments, rule, _, context) {
     bgioArguments.events.setActivePlayers(rule.options)
 
-//     // limited to currentPlayer for now
-//     const phaseName = bgioArguments.ctx.phase
-//     const stageName = bgioArguments.ctx.activePlayers?.[bgioArguments.ctx.currentPlayer]
-//     const phaseOrRoot = context.game.phases?.[phaseName] ?? context.game
-//     const stage = phaseOrRoot?.turn?.stages?.[stageName]
-//     console.log('----------')
-//     console.log('stageName', stageName)
-//     console.log('stage', stage)
-//     console.log('stage.initialMoves', stage.initialMoves)
-
-//     doMoves(bgioArguments, stage?.initialMoves, context)
+    // this is going to need to be expanded to handle more complex things
+    // than "move current player to new stage"
+    const phaseName = bgioArguments.ctx.phase
+    const stageName = rule.options.currentPlayer?.stage
+    const phaseOrRoot = context.game.phases?.[phaseName] ?? context.game
+    const stage = phaseOrRoot?.turn?.stages?.[stageName]
+    doMoves(bgioArguments, stage?.initialMoves, context)
   }
 }
--- server/game-factory/utils/any-valid-moves.js
+++ server/game-factory/utils/any-valid-moves.js
@@ -1,46 +1,4 @@
-
-// import resolveProperties from "./resolve-properties.js";
-// import resolveEntity from "./resolve-entity.js";
-
-// export default function areThereValidMoves(bgioArguments, moves) {
-//   return Object.values(moves).some(move => {
-//     const { moveInstance } = move
-
-//     const context = { moveInstance }
-
-//     const rule = resolveProperties(
-//       bgioArguments,
-//       moveInstance.rule,
-//       context
-//     )
-
-//     // todo: This is incomplete. it is only concerned with the
-//     // validity of one argument at a time and the combination
-//     // of individually valid choices can be invalid. One option
-//     // (maybe the only one) is to try all combinations of valid arguments.
-//     const resolvedPayload = {
-//       arguments: Object.entries(rule.arguments ?? {})
-//         .reduce((acc, [argName, arg]) => {
-//           return {
-//             ...acc,
-//             [argName]: resolveEntity(
-//               bgioArguments,
-//               arg,
-//               context,
-//               argName 
-//             )
-//           }
-//         }, {})
-//     }
-
-//     if (Object.values(resolvedPayload.arguments).every(arg => arg !== undefined)) {
-//       return moveInstance.isValid(bgioArguments, resolvedPayload, context)
-//     } else {
-//       return false
-//     }
-//   })
-// }
-
+import isPlainObject from "lodash/isPlainObject.js";
 import resolveProperties from "./resolve-properties.js";
 import resolveEntity from "./resolve-entity.js";
 
@@ -135,13 +93,16 @@ function findValidCombination(
     moveArguments: currentArgs
   };
   
-  // Get all possible values for this argument
-  const matches = resolveEntity(
-    bgioArguments,
-    { ...arg, matchMultiple: true },
-    updatedContext,
-    argName 
-  );
+  // Get all possible values for this argument if not resolved
+  // If it is unresolved, it means it was a playerChoice
+  const matches = isPlainObject(arg)
+    ? resolveEntity(
+      bgioArguments,
+      { ...arg, matchMultiple: true },
+      updatedContext,
+      argName 
+    )
+    : arg;
   
   const matchArray = Array.isArray(matches) ? matches : (matches !== undefined ? [matches] : []);
   
